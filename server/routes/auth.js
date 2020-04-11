a317a8cda4a7a7fd327cc938507eb13625c2407f
Abhishek Singh <abhi.aryan98@gmail.com>
2020-04-11 21:09:34 +0530

    to tabs?

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -11,42 +11,42 @@ var generateRandomString = (length) => {
   var text = '';
   var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   for (var i = 0; i < length; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
+	text += possible.charAt(Math.floor(Math.random() * possible.length));
   }
   return text;
 };
 
 exp.login = (req, res) => {
-    var state = generateRandomString(16);
+	var state = generateRandomString(16);
 	res.cookie(stateKey, state);
 
-    // your application requests authorization
-    var scope = 'user-read-private user-read-email user-read-playback-state user-modify-playback-state user-read-currently-playing playlist-modify-public user-read-playback-position';
-    res.redirect('https://accounts.spotify.com/authorize?' +
-        querystring.stringify({
-        response_type: 'code',
-        client_id: process.env.SPOTIFY_CLIENT_ID,
-        scope: scope,
-        redirect_uri: process.env.SPOTIFY_REDIRECT_URI,
-        state: state
-    }));
+	// your application requests authorization
+	var scope = 'user-read-private user-read-email user-read-playback-state user-modify-playback-state user-read-currently-playing playlist-modify-public user-read-playback-position';
+	res.redirect('https://accounts.spotify.com/authorize?' +
+		querystring.stringify({
+		response_type: 'code',
+		client_id: process.env.SPOTIFY_CLIENT_ID,
+		scope: scope,
+		redirect_uri: process.env.SPOTIFY_REDIRECT_URI,
+		state: state
+	}));
 }
-    
+	
 exp.callback = (req, res) => {
-    // your application requests refresh and access tokens
-    // after checking the state parameter
-    
-    var code = req.query.code || null;
-    var state = req.query.state || null;
-    var storedState = req.cookies ? req.cookies[stateKey] : null;
-    if (state === null || state !== storedState) {
-        res.redirect('/#' +
-        querystring.stringify({
-            error: 'state_mismatch'
-        }));
-    } else {
-        res.clearCookie(stateKey);
-        var authOptions = {
+	// your application requests refresh and access tokens
+	// after checking the state parameter
+	
+	var code = req.query.code || null;
+	var state = req.query.state || null;
+	var storedState = req.cookies ? req.cookies[stateKey] : null;
+	if (state === null || state !== storedState) {
+		res.redirect('/#' +
+		querystring.stringify({
+			error: 'state_mismatch'
+		}));
+	} else {
+		res.clearCookie(stateKey);
+		var authOptions = {
 			url: 'https://accounts.spotify.com/api/token',
 			form: {
 				code: code,
@@ -57,9 +57,9 @@ exp.callback = (req, res) => {
 				'Authorization': 'Basic ' + (new Buffer(process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET).toString('base64'))
 			},
 			json: true
-        };
-    
-        request.post(authOptions, (error, response, body) => {
+		};
+	
+		request.post(authOptions, (error, response, body) => {
 			if (!error && response.statusCode === 200) {
 				var access_token = body.access_token;
 				var refresh_token = body.refresh_token;
@@ -89,7 +89,7 @@ exp.callback = (req, res) => {
 			}
 		});
 		
-    }
+	}
 }
 
 exp.refresh_token = (req, res) => {
