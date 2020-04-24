f2f8b8948283f11a5711df7bc10fe6562e234a4e
Abhishek Singh <abhi.aryan98@gmail.com>
2020-04-24 17:06:34 +0530

    session storage

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
+import queryString from "query-string";
+import Axios from "axios";
 import "./App.css";
 import Login from "./Login/Login";
 import Player from "./Player/Player";
 import Search from "./Search/Search";
-import queryString from "query-string";
-import Axios from "axios";
 
 class App extends Component {
   constructor(props) {
@@ -13,25 +13,27 @@ class App extends Component {
     this.state = {
       uname: "",
       isUserAuthorized: false,
-    };
+	};
+	
+	sessionStorage.setItem('isLoggedIn', false);
   }
 
   componentDidMount() {
-    const urlParams = queryString.parse(window.location.search);
-    this.setState({ isUserAuthorized: urlParams.authorized === 'true' ? true: false});
+    this.setState({ isUserAuthorized: sessionStorage.getItem('isLoggedIn')});
   }
 
   userLogin = (username) => {
     const url = "http://localhost:8888/login";
     console.log(username);
     this.setState({ uname: username }, () => {
-      Axios.post(url, username)
+    	Axios.post(url, username)
         .then((res) => {
           console.log(res);
         })
         .catch((e) => {
           console.log(e);
-        });
+		});
+		sessionStorage.setItem('isLoggedIn', true);
     });
     window.location.assign("http://localhost:8888/spotify/login");
   };
--- client/src/Login/Login.js
+++ client/src/Login/Login.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
 
 export class Login extends Component {
-    state = {
-        username: ''
+    constructor(props) {
+        super(props);
+        
+        this.state = {
+            username: ''
+        }
     }
 
     handleSubmit = (e) => {
