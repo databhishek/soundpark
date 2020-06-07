299d9812ebf271543292a35ed57f7e0738bd69da
Abhishek Singh <abhi.aryan98@gmail.com>
2020-06-07 18:07:23 +0530

    issue with headers def

--- server/app.js
+++ server/app.js
@@ -19,6 +19,21 @@ redisClient.on('error', (err) => {
 	console.log('Redis error: ', err);
 });
 
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Credentials', true);
+	res.header('Access-Control-Allow-Origin', req.headers.origin);
+	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+	res.header(
+		'Access-Control-Allow-Headers',
+		'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept'
+	);
+	if ('OPTIONS' == req.method) {
+		res.sendStatus(200);
+	} else {
+		next();
+	}
+});
+
 app.use(
 	session({
 		secret: process.env.SESS,
