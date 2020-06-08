cc23f0d300138b3c87ef4435665e8a732eb3c4df
Abhishek Singh <abhi.aryan98@gmail.com>
2020-06-08 19:01:16 +0530

    rollback to old login

--- server/routes/index.js
+++ server/routes/index.js
@@ -25,55 +25,82 @@ module.exports = (passport, io) => {
 	router.post('/playNextReturns', spotify.playNextReturns);
 
 	// Auth Routes
-	router.get('/auth/spotify', auth.redirectSpotify);
-	router.get('/callback', auth.callbackSpotify, (req, res) => {
-		try {
-			// Set interval for refreshing token every hour with id as spotify id
-			userIntervals[req.user.profile.id] = setInterval(async () => {
-				console.log('Refreshing token for user: ' + req.user.profile.id);
-
-				const reqBody = {
-					grant_type: 'refresh_token',
-					refresh_token: req.user.refreshToken
-				};
-
-				const config = {
-					headers: {
-						'Content-Type': 'application/x-www-form-urlencoded',
-						Authorization:
-							'Basic ' +
-							new Buffer.from(
-								process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET
-							).toString('base64')
-					}
-				};
-
-				let resp = await axios.post('https://accounts.spotify.com/api/token', qs.stringify(reqBody), config);
-
-				if (resp.status === 200) {
-					console.log('New access token: ' + resp.data.access_token);
-					db.User.updateOne(
-						{ spotifyID: req.user.profile.id },
-						{
-							$set: {
-								spotifyAccessToken: resp.data.access_token
-							}
-						}
-					);
-				}
-			}, 3600000);
-
-			// Successful authentication, redirect home.
-			console.log('Successful login.');
-			res.redirect(
+	router.get(
+		'/auth/spotify',
+		passport.authenticate('spotify', {
+			scope: [
+				'user-read-private',
+				'user-read-email',
+				'user-read-playback-state',
+				'user-modify-playback-state',
+				'user-read-currently-playing',
+				'playlist-modify-public',
+				'user-read-playback-position'
+			],
+			showDialog: true
+		})
+	);
+	router.get(
+		'/callback',
+		passport.authenticate('spotify', {
+			failureRedirect:
 				process.env.MODE === 'PROD'
-					? process.env.SERVER_URI + '?loggedIn=true'
-					: 'http://localhost:3000/?loggedIn=true'
-			);
-		} catch (e) {
-			console.log(e);
+					? process.env.SERVER_URI + '?loggedIn=false'
+					: 'http://localhost:3000/?loggedIn=false'
+		}),
+		(req, res) => {
+			try {
+				// Set interval for refreshing token every hour with id as spotify id
+				userIntervals[req.user.profile.id] = setInterval(async () => {
+					console.log('Refreshing token for user: ' + req.user.profile.id);
+
+					const reqBody = {
+						grant_type: 'refresh_token',
+						refresh_token: req.user.refreshToken
+					};
+
+					const config = {
+						headers: {
+							'Content-Type': 'application/x-www-form-urlencoded',
+							Authorization:
+								'Basic ' +
+								new Buffer.from(
+									process.env.SPOTIFY_CLIENT_ID + ':' + process.env.SPOTIFY_CLIENT_SECRET
+								).toString('base64')
+						}
+					};
+
+					let resp = await axios.post(
+						'https://accounts.spotify.com/api/token',
+						qs.stringify(reqBody),
+						config
+					);
+
+					if (resp.status === 200) {
+						console.log('New access token: ' + resp.data.access_token);
+						db.User.updateOne(
+							{ spotifyID: req.user.profile.id },
+							{
+								$set: {
+									spotifyAccessToken: resp.data.access_token
+								}
+							}
+						);
+					}
+				}, 3600000);
+
+				// Successful authentication, redirect home.
+				console.log('Successful login.');
+				res.redirect(
+					process.env.MODE === 'PROD'
+						? process.env.SERVER_URI + '?loggedIn=true'
+						: 'http://localhost:3000/?loggedIn=true'
+				);
+			} catch (e) {
+				console.log(e);
+			}
 		}
-	});
+	);
 
 	return router;
 };
