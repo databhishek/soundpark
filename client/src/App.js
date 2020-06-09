0826870da2c2cf607e4c63d6d1c86c7c71ab8284
Abhishek Singh <abhi.aryan98@gmail.com>
2020-06-10 01:44:23 +0530

    put components inside folder

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 import './App.scss';
-import Header from './Header';
-import Footer from './Footer';
-import Home from './Home/Home';
-import Player from './Player/Player';
-import CreateRoom from './CreateRoom/CreateRoom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Home from './components/Home/Home';
+import Player from './components/Player/Player';
+import CreateRoom from './components/CreateRoom/CreateRoom';
 
 class App extends Component {
 	render() {
