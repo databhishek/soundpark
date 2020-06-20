57681d8fce924f367f2d133a5b8384c711ee2a49
Arav Saraff <aravsaraff@gmail.com>
2020-06-21 01:13:53 +0530

    Typo

--- server/config/passport.js
+++ server/config/passport.js
@@ -17,7 +17,7 @@ module.exports = (passport) => {
 			{
 				clientID: process.env.SPOTIFY_CLIENT_ID,
 				clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
-				callbackURL: 'http://soundpark.live/api/callback'
+				callbackURL: 'https://soundpark.live/api/callback'
 			},
 			(accessToken, refreshToken, expires_in, profile, done) => {
 				db.User.findOneAndUpdate(
